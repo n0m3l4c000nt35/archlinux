02be532dfa4d8bfec24dcc6b1ed0748794fdd1ed
Esteban Zárate <zar23001@byui.edu>
2025-06-18 21:50:21 -0300

    Create target_to_hack.sh

---
+++ files/target_to_hack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ip_address=$(/bin/cat $HOME/.config/polybar/scripts/target.txt)
+
+if [ -n "$ip_address" ]; then
+  echo "%{T2}%{F#ff0000}󰓾%{T-} %{F#fff}$ip_address"
+else
+  echo ""
+fi
